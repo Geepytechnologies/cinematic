109d437580adab26af364625085cc610be97bbf4
geepy technologies <geepytechnologies@gmail.com>
2023-03-07 23:44:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+const mypath = path.join(__dirname, "../frontend/dist");
+console.log(mypath);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+const PORT = process.env.PORT || 3000;
+mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("DB Connection Successfull"))
+//   .then(() =>
+//     app.listen(PORT, () => {
+//       console.log("Backend server is Running");
+//     })
+//   )
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// Serve the React app for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
+app.get("/test", (req, res) => {
+  res.send("hello");
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Backend server is Running");
+  });
+});
+// app.listen(PORT, () => {
+//   console.log("Backend server is Running");
+// });
+
+module.exports = app;
---
+++ routes/movies.js
@@ -0,0 +1,149 @@
+const router = require("express").Router();
+const Movie = require("../models/Movie");
+const verify = require("../verifyToken");
+
+//CREATE
+
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (err) {
+      // res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//UPDATE
+
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//DELETE
+
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.params.id);
+      res.status(200).json("The movie has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//GET
+
+router.get("/find/:id", verify, async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//GET
+
+router.get("/", verify, async (req, res) => {
+  const typeQuery = req.query.type;
+  const genreQuery = req.query.genre;
+  let movie = [];
+  try {
+    if (typeQuery) {
+      if (genreQuery) {
+        movie = await Movie.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery, genre: genreQuery } },
+        ]);
+      } else {
+        movie = await Movie.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery } },
+        ]);
+      }
+    } else {
+      movie = await Movie.find();
+      res.status(200).json(movie);
+    }
+  } catch (err) {
+    // res.status(500).json(err);
+  }
+});
+
+//GET RANDOM
+
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: true } },
+        // { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: false } },
+        // { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    // res.status(500).json(err);
+  }
+});
+
+// GET RANDOM BY GENRE
+router.get("/random/genre", verify, async (req, res) => {
+  const genre = req.query.genre;
+  let movie;
+  try {
+    if (genre) {
+      movie = await Movie.aggregate([{ $match: { genre: genre } }]);
+    } else {
+      movie = await Movie.find();
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    throw err;
+  }
+});
+
+//GET ALL
+
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const movies = await Movie.find();
+      res.status(200).json(movies.reverse());
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+module.exports = router;
